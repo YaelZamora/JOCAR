a692e4df081ba210dae5997b80f571dfb5c5f5d8
YaelZamora <ajedrezkasparov@gmail.com>
2026-06-19 10:14:38 -0600

    Screens and Constants

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFF0046CC);
+const secondary = Color(0xFF00F2FF);
+const tertiary = Color(0xFF00A89F);
+const neutral = Color(0xFF1A1C1E);
---
+++ lib/screens/ingenieria_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IngenieriaScreen extends StatelessWidget {
+  const IngenieriaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
